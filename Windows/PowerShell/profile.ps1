515f15b44ed7d0302f4542c7353df43dfe9356ac
Hsin-Hsiang Peng <hsinspeng@gmail.com>
2020-11-18 17:03:59 +0800

    Create profile.ps1

---
+++ Windows/PowerShell/profile.ps1
@@ -0,0 +1,9 @@
+#region conda initialize
+# !! Contents within this block are managed by 'conda init' !!
+(& "C:\ProgramData\Anaconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
+#endregion
+
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module windows-screenfetch
+Set-Theme Honukai
